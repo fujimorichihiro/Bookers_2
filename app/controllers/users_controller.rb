fd7671183fce12c5f8d57ac85afd377b7977bdf0
fujimorichihiro <fujimori.drum@gmail.com>
2020-05-18 17:17:40 +0900

    Add follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,13 @@ class UsersController < ApplicationController
         render :edit
     end
   end
+
+  def following
+  end
+
+  def followers
+  end
+  
   private 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   root  'homes#top'
   get 'home/about' => 'homes#show'
 # books,users,post_comments,favorites
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+  	member do
+  		get :following, :followers
+  	end
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 end
