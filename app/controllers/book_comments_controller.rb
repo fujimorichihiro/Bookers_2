c75b6013ec4dfdc4ec8804dbcc91e767ee1fd6a4
fujimorichihiro <fujimori.drum@gmail.com>
2020-05-20 14:22:48 +0900

    finish comment-ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,30 +1,26 @@
 class BookCommentsController < ApplicationController
-
+# もっと短く書けないかメンターに聞く。どのデータが使えるかなど
   def create
   	@showbook = Book.find(params[:book_id])
   	@book_comment = BookComment.new(book_comment_params)
   	@book_comment.user_id = current_user.id
   	@book_comment.book_id = @showbook.id
-  	if @book_comment.save
-  		redirect_to book_path(@showbook)
-  	else
-  		@book = Book.new
-  		@user = User.find(@showbook.user_id)
-  	    render 'books/show'
-  	end
+  	@book_comment.save
+  	render 'create'
   end
 
   def destroy
-  	book = Book.find(params[:book_id])
-  	book_comment = current_user.book_comments.find_by(book_id: book.id)
-  	book_comment.destroy
-  	redirect_to book_path(book)
+  	# book = Book.find(params[:book_id])
+  	# book_comment = current_user.book_comments.find_by(book_id: book.id)
+  	@book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+  	render 'destroy'
   end
 
   private
 
   def book_comment_params
-  	params.require(:book_comment).permit(:comment)
+  	params.require(:book_comment).permit(:comment, :user_id, :book_id)
   end
 
 end
